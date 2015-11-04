2e54ed28c3969368a2185747e34dd16b6007512d
Benjamin <deslandes.benjamin@live.fr>
2015-11-04 17:31:43 +0100

    Ajout de activity_alarm.xml

--- app/src/main/java/com/example/benjamin/thief_catcher/MainActivity.java
+++ app/src/main/java/com/example/benjamin/thief_catcher/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.benjamin.thief_catcher;
 
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
